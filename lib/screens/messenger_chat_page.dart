5be05b35ed100f0954dc629923a36a8bd7921df7
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-05 11:53:25 +0900

    Messenger App Part 2

--- lib/screens/messenger_chat_page.dart
+++ lib/screens/messenger_chat_page.dart
@@ -13,6 +13,7 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // resizeToAvoidBottomInset: false,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -20,7 +21,7 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
             Expanded(
               flex: 3,
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: Row(
                   children: [
                     Container(
@@ -40,12 +41,38 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
                     ),
                     Expanded(
                       child: Column(
-                        children: [],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Design Team',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(2.0),
+                            child: Text('8 members, 5 online'),
+                          ),
+                        ],
                       ),
                     ),
                     Container(
                       height: 42,
                       width: 42,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                        child: Text(
+                          'UI',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
                     ),
                   ],
                 ),
@@ -56,20 +83,153 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
               thickness: 1.5,
             ),
             Expanded(
-              flex: 25,
+              flex: 28,
               child: SingleChildScrollView(
                 child: Column(
                   children: [
                     SizedBox(
-                      height: MediaQuery.of(context).size.height / 1.4,
-                      child: ListView(),
+                      height: MediaQuery.of(context).size.height / 1.38,
+                      child: ListView(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 8),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 8),
+                                  child: CircleAvatar(
+                                    radius: 30,
+                                    backgroundImage: NetworkImage('https://i.pravatar.cc/300'),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      children: [
+                                        Container(
+                                          padding: EdgeInsets.all(8.0),
+                                          decoration: BoxDecoration(
+                                            border:
+                                                Border.all(color: Colors.grey[300]!),
+                                            borderRadius: BorderRadius.only(
+                                              topRight: Radius.circular(8),
+                                              bottomLeft: Radius.circular(8),
+                                              bottomRight: Radius.circular(8),
+                                            ),
+                                          ),
+                                          child: Column(
+                                            children: [
+                                              Text(
+                                                'https://dribbble.com/shots/15474874-Messenger-Mobile-Version/attachments/7248290?mode=media',
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                  color: Colors.grey,
+                                                  fontSize: 16,
+                                                  decoration:
+                                                      TextDecoration.underline,
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    const EdgeInsets.all(8.0),
+                                                child: Row(
+                                                  children: [
+                                                    Container(
+                                                      width: 4,
+                                                      decoration: BoxDecoration(
+                                                        color: Colors.blue,
+                                                      ),
+                                                    ),
+                                                    Expanded(
+                                                      child: Column(
+                                                        crossAxisAlignment:
+                                                            CrossAxisAlignment
+                                                                .start,
+                                                        children: [
+                                                          Text(
+                                                            'Dribble',
+                                                            style: TextStyle(
+                                                              color:
+                                                                  Colors.blue,
+                                                            ),
+                                                          ),
+                                                          Padding(
+                                                            padding:
+                                                                const EdgeInsets
+                                                                        .symmetric(
+                                                                    vertical:
+                                                                        4),
+                                                            child: Text(
+                                                              'Banking UI kit for Figma',
+                                                              style: TextStyle(
+                                                                fontWeight:
+                                                                    FontWeight
+                                                                        .bold,
+                                                                color: Colors
+                                                                    .black,
+                                                              ),
+                                                            ),
+                                                          ),
+                                                          Text(
+                                                            'Banking UI kit for Figma. Designed by Nolan Mango for ZIPL Web Studio. Connect with them on Dribble - the global community for designers and creative professionals.',
+                                                            style: TextStyle(
+                                                              color: Colors
+                                                                  .grey[400],
+                                                            ),
+                                                          ),
+                                                          Padding(
+                                                            padding:
+                                                                const EdgeInsets
+                                                                        .symmetric(
+                                                                    vertical:
+                                                                        4),
+                                                            child: Image.network(
+                                                                'https://cdn.dribbble.com/users/900341/screenshots/15474874/media/b6afef9541a56d5d95b13a8ee0b57f00.jpg'),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Align(
+                                          alignment: Alignment.centerRight,
+                                          child: Padding(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 4),
+                                            child: Text(
+                                              '9:45',
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: Colors.grey,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                     Divider(
                       color: Colors.grey,
                       thickness: 1.5,
                     ),
                     SizedBox(
-                      height: 50,
+                      height: 55,
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Row(
@@ -83,6 +243,7 @@ class _MessengerChatPageState extends State<MessengerChatPage> {
                                 controller: _textEditingController,
                                 decoration: InputDecoration(
                                   hintText: "Write a message",
+                                  border: InputBorder.none,
                                 ),
                                 onSubmitted: (value) {
                                   _textEditingController.clear();
